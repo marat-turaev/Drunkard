84655d9fb2e762fed9c91b596751fed0dd07450d
Marat Turaev <marat.turaev@hotmail.com>
2014-06-03 00:42:51 +0400

    refactoring

--- src/ru/spbau/turaev/drunkard/Main.java
+++ src/ru/spbau/turaev/drunkard/Main.java
@@ -1,11 +1,11 @@
 package ru.spbau.turaev.drunkard;
 
+import ru.spbau.turaev.drunkard.domain.HexagonalMap;
 import ru.spbau.turaev.drunkard.domain.Map;
-import ru.spbau.turaev.drunkard.domain.RectangleMap;
 
 public class Main {
     public static void main(String[] args) {
-        Map map = new RectangleMap(15, 15);
+        Map map = new HexagonalMap(15, 15);
 
         for (int i = 0; i < 500; i++) {
             map.doStep();
